95a9141b1efc033501863a7c28dd1c17316d4175
Emmett Alyea <ealyea2028@spyponders.com>
2024-12-03 15:27:37 -0500

    finnished Lift Kit - prob

--- LiftTest2425.java
+++ LiftTest2425.java
@@ -18,6 +18,12 @@ public class LiftTest2425 extends LinearOpMode {
     private int liftMaxHeight; //upperbound of liftkit position
     private int liftMinimumHeight; //lower bound of liftkit position- might be unneeded if equals 0
     private int saveElapsedMilli; //used for equations like: elapesed time = total time - time since I set this variable
+    private boolean dPadUpPressed;
+    private boolean dPadDownPressed;
+    private boolean dPadLeftPressed;
+    private boolean dPadRightPressed;
+    private boolean aPressed;
+    private boolean yPressed;
 
     @Override
     public void runOpMode(){
@@ -30,21 +36,12 @@ public class LiftTest2425 extends LinearOpMode {
         }, //TODO: make something that can find/update the directions
                 new boolean[] {true,true,true,true}
         );
-        clawArm = new ClawArm2425(hardwareMap.get(DcMotor.class, "armMotor"));
-        //TODO: find openPos and closedPos
-        claw = new Claw2425(0, 0, hardwareMap.get(Servo.class, "clawServo"));
         //variable initialize - variables
         liftSpeed = 0.5;
         liftMaxHeight = 100000;
         liftMinimumHeight = 0;
-        moveSpeed = 1;
-        minTranslatePower = 0.25;
-        clawArmSpeed = 0.2;
         ElapsedTime runtime = new ElapsedTime();
         runtime.reset();
-
-        boolean rBumpPressed = false;
-
         // Tells the driver that the robot is ready
         telemetry.addData("Status", "Initialized");
         telemetry.update();
@@ -76,41 +73,53 @@ public class LiftTest2425 extends LinearOpMode {
                 liftKit.changeTargetHeight(rightY * liftSpeed * timeCoef);
             }
             //lift kit calibration
+            //liftSpeed liftMaxHeight liftMinHeight
+            if (gamepad1.dpad_up && !dPadUpPressed){
+                dPadUpPressed = true;
+                liftSpeed+= 0.025;
+            }else if (!gamepad1.dpad_up){
+                dPadUpPressed = false;
+            }
+            if (gamepad1.dpad_down && !dPadDownPressed){
+                dPadDownPressed = true;
+                liftSpeed-=0.025;
+            }else if (!gamepad1.dpad_down){
+                dPadDownPressed = false;
+            }
+
+
+            if (gamepad1.dpad_right && !dPadRightPressed){
+                dPadRightPressed = true;
+                liftMaxHeight +=1000;
+            }else if (!gamepad1.dpad_right){
+                dPadRightPressed = false;
+            }
+            if (gamepad1.dpad_left && !dPadLeftPressed){
+                dPadLeftPressed = true;
+                liftMaxHeight +=1000;
+            }else if (!gamepad1.dpad_left){
+                dPadLeftPressed = false;
+            }
+
+
+
+            if (gamepad1.a && !aPressed){
+                aPressed = true;
+                liftMinimumHeight +=1000;
+            }else if (!gamepad1.a){
+                aPressed = false;
+            }
+            if (gamepad1.y && !yPressed){
+                yPressed = true;
+                liftMinimumHeight+=1000;
+            }else if (!gamepad1.y){
+                yPressed = false;
+            }
+
+
             liftKit.powerMotors();
 
-            //<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<,drive train control
-            double leftX = gamepad1.left_stick_x;
-            double leftY = gamepad1.left_stick_y;
-            driveTrain.translate(calcTranslatePower(leftX),calcTranslatePower(leftY));
 
-            double leftTrigger = gamepad1.left_trigger;
-            double rightTrigger = gamepad1.right_trigger;
-            driveTrain.rotate(calcRotatePower(leftTrigger), calcRotatePower(rightTrigger));
-
-            //<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< claw stuff
-            if (gamepad1.right_bumper && !rBumpPressed) {
-                claw.toggleClaw();
-                rBumpPressed = true;
-            } else if (!gamepad1.right_bumper) {
-                rBumpPressed = false;
-            }
-
-            //<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< arm stuff
-            if (gamepad1.y) {
-                clawArm.changeTargetPosition(clawArmSpeed*timeCoef);
-            } else if (gamepad1.b){
-                clawArm.changeTargetPosition(-1*clawArmSpeed*timeCoef);
-            }
-            clawArm.powerArm();
-
-            //<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< telemetry
-            telemetry.update();
         }
-    }
-    public double calcTranslatePower (double stickPos) {
-        return (stickPos*(1-minTranslatePower)+Math.signum(stickPos)*minTranslatePower)*moveSpeed;
-    }
-    public double calcRotatePower (double triggerPos) {
-        return triggerPos;
     }
 }
