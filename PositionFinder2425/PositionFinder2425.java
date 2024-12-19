640327f6965d3f65ba5c0c761c6543c04ad8d926
Anton Rie <riea50206@gmail.com>
2024-12-19 15:16:59 -0500

    Additions to the Position Finder Class.

--- PositionFinder2425/PositionFinder2425.java
+++ PositionFinder2425/PositionFinder2425.java
@@ -1,18 +1,43 @@
 package org.firstinspires.ftc.teamcode.Automotons2425.PositionFinder2425;
-
 import org.firstinspires.ftc.teamcode.Automotons2425.DriveTrain2425.DriveTrain2425;
 import org.firstinspires.ftc.teamcode.Automotons2425.Position;
 
 /* Based entirely on */
 public class PositionFinder2425 {
-    DriveTrain2425 driveTrain2425;
-    //TODO: not done
-    public PositionFinder2425 (DriveTrain2425 driveTrain, Position position) {
+    DriveTrain2425 driveTrain;
+    Position currentPosition;
 
+    public PositionFinder2425 (DriveTrain2425 driveTrain, Position position) {
+        this.driveTrain = driveTrain;
+        currentPosition = position;
     }
-    // TODO: Not done
     /** Returns the current position of the robot*/
-    public Position getPosition (){
-        return new Position(0,0,0);
+    public Position getPosition () {
+        return currentPosition;
+    }
+    /**
+     * Sets the current running position to a specified position
+     * @param position The new position
+     */
+    public void setPosition (Position position) {
+        currentPosition = new Position(position);
+    }
+    /**
+     * Updates the position by adding the current running position to the parameters
+     * @param xDistance The distance in the x direction the drive train has moved since last updating
+     * @param yDistance The distance in the y direction the drive train has moved since last updating
+     * @param angle The angle (in radians) the robot has rotated since last updating
+     */
+    private void adjustPosition (double xDistance, double yDistance, double angle) {
+        Position newPosition = new Position(
+                currentPosition.aPos+xDistance,
+                currentPosition.bPos+yDistance,
+                currentPosition.facingDirection+angle
+        );
+        currentPosition = new Position(newPosition);
+    }
+    public void updatePosition () {
+        Position changeInPosition = driveTrain.positionChange();
+        adjustPosition(changeInPosition.aPos, changeInPosition.bPos, changeInPosition.facingDirection);
     }
 }
