8bc095e74109162b4d3f7b4a99a1a3afd7e08201
Emmett Alyea <ealyea2028@spyponders.com>
2025-01-02 15:20:06 -0500

    Added rotation matrix to adjustPositionRobotSpace so it doesn't translate as if the robot is always facing strait in the positive bPos direction.

--- PositionFinder2425/PositionFinder2425.java
+++ PositionFinder2425/PositionFinder2425.java
@@ -28,7 +28,7 @@ public class PositionFinder2425 {
      * @param yDistance The distance in the y direction the drive train has moved since last updating
      * @param angle The angle (in radians) the robot has rotated since last updating
      */
-    private void adjustPosition (double xDistance, double yDistance, double angle) {
+    private void adjustPositionAbsoluteSpace (double xDistance, double yDistance, double angle) {
         Position newPosition = new Position(
                 currentPosition.aPos+xDistance,
                 currentPosition.bPos+yDistance,
@@ -36,8 +36,24 @@ public class PositionFinder2425 {
         );
         currentPosition = new Position(newPosition);
     }
+    /**
+     * Updates the position by adding the current running position to the parameters
+     * multiplies xDistance and yDistance by current angle rotation matrix to transform into absolute space
+     * @param xDistance The distance in the x direction the drive train has moved since last updating
+     * @param yDistance The distance in the y direction the drive train has moved since last updating
+     * @param angle The angle (in radians) the robot has rotated since last updating
+     */
+    private void adjustPositionRobotSpace (double xDistance, double yDistance, double angle) {
+        Position newPosition = new Position(
+                currentPosition.aPos+xDistance*Math.cos(currentPosition.facingDirection) - yDistance * Math.sin(currentPosition.facingDirection),
+                currentPosition.bPos+xDistance*Math.sin(currentPosition.facingDirection) + yDistance * Math.cos(currentPosition.facingDirection),
+                currentPosition.facingDirection+angle
+        );
+        currentPosition = new Position(newPosition);
+    }
+
     public void updatePosition () {
         Position changeInPosition = driveTrain.positionChange();
-        adjustPosition(changeInPosition.aPos, changeInPosition.bPos, changeInPosition.facingDirection);
+        adjustPositionRobotSpace(changeInPosition.aPos, changeInPosition.bPos, changeInPosition.facingDirection);
     }
 }
