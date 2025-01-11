fb1e5a0a9969536564c70565d9b37c769fad46b7
Emmett Alyea <ealyea2028@spyponders.com>
2025-01-11 09:41:13 -0500

    set up basic TeleOp structure in all ActionTest2425 files

--- ActionsTest2425/ArmActionTest2425.java
+++ ActionsTest2425/ArmActionTest2425.java
@@ -1,4 +1,25 @@
 package org.firstinspires.ftc.teamcode.Automotons2425.ActionsTest2425;
 
-public class ArmActionTest2425 {
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+
+@TeleOp(name = "ArmActionTest2425", group = "Linear OpMode")
+public class ArmActionTest2425 extends LinearOpMode {
+
+
+    @Override
+    public void runOpMode () {
+        telemetry.update();
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+
+
+        while (opModeIsActive()) {
+
+
+            telemetry.update();
+        }
+    }
+
 }
