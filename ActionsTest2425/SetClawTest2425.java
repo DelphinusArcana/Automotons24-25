47e4c91f6ce58fe7b18dc3a7edcd930a39139e9f
Emmett Alyea <ealyea2028@spyponders.com>
2025-01-11 10:33:04 -0500

    added telemetry

--- ActionsTest2425/SetClawTest2425.java
+++ ActionsTest2425/SetClawTest2425.java
@@ -15,11 +15,12 @@ import java.util.ArrayList;
 public class SetClawTest2425 extends LinearOpMode  {
     Claw2425 Claw;
     double saveElapsedMilli;
+    boolean clawState;
     @Override
     public void runOpMode () {
-
+        clawState = true;
         Claw = new Claw2425(0, 0, hardwareMap.get(Servo.class, "clawServo"));
-        
+
         ArrayList<SetClaw2425> actionList = new ArrayList<SetClaw2425>();
 
         actionList.add(new SetClaw2425(Claw, true));
@@ -42,12 +43,15 @@ public class SetClawTest2425 extends LinearOpMode  {
             actionList.get(0).doAction();
 
             if (runtime.milliseconds()-saveElapsedMilli == 1000){
-                saveElapsedMilli = runtime.milliseconds();
+                telemetry.speak("1 second has passed");
                 if (actionList.get(0).isComplete()) {
-                   actionList.remove(0);
+                    saveElapsedMilli = runtime.milliseconds();
+                    actionList.remove(0);
+                    telemetry.speak("Toggling claw");
+                    clawState = !clawState;
+                    telemetry.addData("Claw action is",clawState);
                 }
             }
-
 
             telemetry.update();
         }
