9669ffc3c0b494406b9eb3bc7cb12da2b0b2546e
Anton Rie <riea50206@gmail.com>
2025-02-04 14:40:44 -0500

    Changed main function of ServoSim.java to runTests() and changed fake telemetry to a passed in telemetry.

--- TestingClasses/ServoSim.java
+++ TestingClasses/ServoSim.java
@@ -9,7 +9,7 @@ public class ServoSim extends ServoWrapper {
 
     /** CONSTRUCTOR sets all instance variables
      * @param startPosition the starting position for this
-     * @param name this's name*/
+     * @param name this's name */
     public ServoSim(double startPosition, String name) {
         super (FakeHardware.fakeServo, startPosition, name);
     }
@@ -17,17 +17,16 @@ public class ServoSim extends ServoWrapper {
     public void printInfo (Telemetry telemetry) {
         telemetry.addData("ServoSim " + name, "Position:" + position);
     }
-    public static void main(String[] args){
-        fakeTelemetry = new FakeTelemetry();
+    public static void runTests(Telemetry telemetry){
         ServoSim servoSim1 = new ServoSim(0, "servoSim1");
-        servoSim1.printInfo(fakeTelemetry.fake);
+        servoSim1.printInfo(telemetry);
         servoSim1.getName();
         servoSim1.setPosition(1000.0);
-        servoSim1.printInfo(fakeTelemetry.fake);
+        servoSim1.printInfo(telemetry);
         for(double i = 0.0; i>10000; i+=0.3){
             servoSim1.setPosition(i);
-            servoSim1.printInfo(fakeTelemetry.fake);
-            fakeTelemetry.fake.addData("correct position value ",  i);
+            servoSim1.printInfo(telemetry);
+            telemetry.addData("correct position value ",  i);
         }
     }
 }
