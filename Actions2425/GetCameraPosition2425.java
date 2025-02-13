c1f0fa0bcced951f5446d8dd53e57e84505105bf
Emmett Alyea <ealyea2028@spyponders.com>
2025-02-13 15:39:38 -0500

    changed name to match naming style

---
+++ Actions2425/GetCameraPosition2425.java
@@ -0,0 +1,54 @@
+package org.firstinspires.ftc.teamcode.Automotons2425.Actions2425;
+
+import org.firstinspires.ftc.teamcode.Automotons2425.AprilTag;
+import org.firstinspires.ftc.teamcode.Automotons2425.PositionFinder2425.CameraPositionFinder2425;
+
+public class GetCameraPosition2425 implements Action2425 {
+    CameraPositionFinder2425 positionFinder;
+    AprilTag[] aprilTags;
+    double endCameraPos;
+    double endCameraPosError;
+    boolean isComplete;
+    double cameraStartPos;
+    boolean doSetupCamera;
+    FakeCameraClass2425 camera;
+    //anythin involving camera is just placement
+    public GetCameraPosition2425(CameraPositionFinder2425 cPositionFinder, FakeCameraClass2425 cameraArg){
+        positionFinder = cPositionFinder;
+        camera = cameraArg;
+        //magic # represents the end position of the camera's servo
+        endCameraPos = 1;
+        //magic # represents error for endCameraPos
+        endCameraPosError = 0.05;
+        //magic # for resetting camera servo at end of action
+        cameraStartPos = -1;
+        isComplete = false;
+        doSetupCamera = true;
+
+    }
+
+    //todo: idk how to do this
+    @Override
+    public boolean isComplete() {
+        return isComplete;
+    }
+
+    @Override
+    public void doAction() {
+        //clear camera if it is the start of the action to remove previously seen tags with incorrect positions
+        if (doSetupCamera){
+            doSetupCamera = false;
+            camera.clearSeen();
+        }
+        //checks if cameraServo has scanned all 240 or so degrees
+        if(camera.servoPosition >endCameraPos-endCameraPosError){
+            positionFinder.useCameraPositon(camera.getSeen());
+            isComplete = true;
+            //resets camera position
+            camera.setServoPosition(cameraStartPos);
+        }
+        else{
+            camera.setServoPosition(endCameraPos);
+        }
+    }
+}
