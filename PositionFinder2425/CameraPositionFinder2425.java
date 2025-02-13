e1bd220535c52273f71a09305e2585cf310d0b01
Emmett Alyea <ealyea2028@spyponders.com>
2025-02-13 15:40:23 -0500

    now accepts a List<AprilTagDetection> as an argument in useCameraPosition and cameraCalc

--- PositionFinder2425/CameraPositionFinder2425.java
+++ PositionFinder2425/CameraPositionFinder2425.java
@@ -2,6 +2,9 @@ package org.firstinspires.ftc.teamcode.Automotons2425.PositionFinder2425;
 
 import org.firstinspires.ftc.teamcode.Automotons2425.DriveTrain2425.DriveTrain2425;
 import org.firstinspires.ftc.teamcode.Automotons2425.Position;
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
+
+import java.util.List;
 
 public class CameraPositionFinder2425 extends PositionFinder2425{
     //todo: add a camera class to constructor
@@ -10,12 +13,12 @@ public class CameraPositionFinder2425 extends PositionFinder2425{
     }
     //todo: Anton, this is where you will add your calculation code
     //todo: add camera info in the paramaters
-    private Position cameraCalc() {
+    private Position cameraCalc(List<AprilTagDetection> seenTags) {
         Position callThisWhateverYouWantAnton = new Position(0, 0, 0);
         return callThisWhateverYouWantAnton;
     }
     //todo: this needs to take arguments but we don't have the data type for that argument
-    public void useCameraPositon(/*argument example*/){
-        currentPosition = cameraCalc(/*argument example*/);
+    public void useCameraPositon(List<AprilTagDetection> seenTags){
+        currentPosition = cameraCalc(seenTags);
     }
 }
