9d15c7e271a10e7ceba6378037209e7258d39962
Emmett Alyea <ealyea2028@spyponders.com>
2025-02-15 12:25:58 -0500

    did some stuff for cameraCalc

--- PositionFinder2425/CameraPositionFinder2425.java
+++ PositionFinder2425/CameraPositionFinder2425.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode.Automotons2425.PositionFinder2425;
 
 import com.qualcomm.robotcore.util.ElapsedTime;
 
+import org.firstinspires.ftc.teamcode.Automotons2425.AprilTag;
 import org.firstinspires.ftc.teamcode.Automotons2425.Camera2425.Camera2425;
 import org.firstinspires.ftc.teamcode.Automotons2425.DriveTrain2425.DriveTrain2425;
 import org.firstinspires.ftc.teamcode.Automotons2425.Position;
@@ -15,18 +16,29 @@ public class CameraPositionFinder2425 extends PositionFinder2425{
     private Camera2425 camera;
     private ElapsedTime runtime;
     private int lastMilli;
+    private ArrayList<AprilTag> aprilTags;
     public CameraPositionFinder2425(DriveTrain2425 driveTrain, Position position, Camera2425 camera) {
         super(driveTrain, position);
         this.camera = camera;
         doingSweep = false;
         runtime = new ElapsedTime();
         lastMilli = (int) runtime.milliseconds();
+        aprilTags = AprilTag.getDefaultArena2425();
     }
-    //todo: Anton, this is where you will add your calculation code
-    //todo: add camera info in the paramaters
     private Position cameraCalc(ArrayList<TagDetection> seenTags) {
-        Position callThisWhateverYouWantAnton = new Position(0, 0, 0);
-        return callThisWhateverYouWantAnton;
+        ArrayList uniqueTags = filterDetections(seenTags);
+        if(uniqueTags.size()>1){
+            AprilTag tag1 = aprilTags.get();
+            //this site says that this is the math. idk
+            //https://math.stackexchange.com/questions/256100/how-can-i-find-the-points-at-which-two-circles-intersect#:~:text=The%20intersection%20points%20are%20given,the%20centers%20of%20the%20circles.
+            //(x,y)=12(x1+x2,y1+y2)+r21−r222R2(x2−x1,y2−y1)±12√2r21+r22R2−(r21−r22)2R4−1(y2−y1,x1−x2)
+
+        }
+
+        //return ;
+    }
+    private AprilTag findAprilTag(int tagID){
+        for(aprilTags:)
     }
     public void useCameraPositon(ArrayList<TagDetection> seenTags){
         currentPosition = cameraCalc(seenTags);
