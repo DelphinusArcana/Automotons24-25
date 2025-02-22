5788413170bbc12f60fa2176d6e18f328253de09
Emmett Alyea <ealyea2028@spyponders.com>
2025-02-22 10:26:42 -0500

    added restrictions on movement if target position has not been changed recently

--- ClawArm2425/ClawArm2425.java
+++ ClawArm2425/ClawArm2425.java
@@ -4,6 +4,7 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 
 public class ClawArm2425 {
@@ -30,8 +31,12 @@ public class ClawArm2425 {
     private int maxPowerError;
     /** The maximum power to send to the motors */
     private double maxPower;
+    private double timeSinceInput;
     private boolean direction;
+    private double updateTimeMoveLimitCoeficient;
+    int previousMili;
     private int prevMotorPosition;
+    private ElapsedTime elapsedTime;
     /**
      * CONSTRUCTOR
      Sets all instance variables
@@ -47,6 +52,10 @@ public class ClawArm2425 {
         prevMotorPosition = zeroPosition;
         maxPowerError = 5;
         maxPower = 0.5;
+        previousMili = 0;
+        elapsedTime = new ElapsedTime();
+        //todo find this
+        updateTimeMoveLimitCoeficient = 10;
         direction = true;
         updateDirection();
     }
@@ -54,7 +63,10 @@ public class ClawArm2425 {
      * Mutator method to set the arm’s target position
      */
     public void setTargetPosition (double position) {
+        double changeInTargetPosition = position-targetPosition;
+        timeSinceInput = -1*Math.abs(changeInTargetPosition)*updateTimeMoveLimitCoeficient;
         targetPosition = position;
+
         /*if (targetPosition < uprightPosition){
             targetPosition = uprightPosition;
         }
@@ -80,12 +92,20 @@ public class ClawArm2425 {
      */
     public void powerArm (Telemetry telemetry) {
         //motor.setPower(maxPower);
+        int elapsedMili = (int) elapsedTime.milliseconds()-previousMili;
+        previousMili = (int) elapsedTime.milliseconds();
         //motor.setTargetPosition((int) targetPosition);
         int currentPosition = motor.getCurrentPosition();
         if (Math.abs(currentPosition - prevMotorPosition) > 10) {
             int offset = currentPosition - prevMotorPosition;
             targetPosition += offset;
         }
+        timeSinceInput += elapsedMili;
+        if (timeSinceInput>0){
+            int offset = currentPosition - prevMotorPosition;
+            targetPosition += offset;
+        }
+
 
         double error = targetPosition - motor.getCurrentPosition();
         double power = 0;
