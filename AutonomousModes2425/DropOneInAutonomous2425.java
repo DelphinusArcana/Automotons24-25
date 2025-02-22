f8300fd9e06fa85099cb7bdccb6aea5b011f9d36
Anton Rie <riea50206@gmail.com>
2025-02-22 08:51:31 -0500

    Changed the claw instructions to be spinClaw instead.

--- AutonomousModes2425/DropOneInAutonomous2425.java
+++ AutonomousModes2425/DropOneInAutonomous2425.java
@@ -4,12 +4,15 @@ import org.firstinspires.ftc.teamcode.Automotons2425.Actions2425.Action2425;
 import org.firstinspires.ftc.teamcode.Automotons2425.Actions2425.GoToPosition2425;
 import org.firstinspires.ftc.teamcode.Automotons2425.Actions2425.SetArm2425;
 import org.firstinspires.ftc.teamcode.Automotons2425.Actions2425.SetClaw2425;
+import org.firstinspires.ftc.teamcode.Automotons2425.Actions2425.SetSpinClaw2425;
 import org.firstinspires.ftc.teamcode.Automotons2425.Actions2425.SetLift2425;
 import org.firstinspires.ftc.teamcode.Automotons2425.Position;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
 import java.util.ArrayList;
 
-public class DropOneInAutonomus2425 extends AbstractAutonomous{
+@Autonomous(name="DropOneInAutonomous2425")
+public class DropOneInAutonomous2425 extends AbstractAutonomous{
     private double oritentationTolerance;
     private double moveTolerance;
     private int armTolerance;
@@ -19,8 +22,8 @@ public class DropOneInAutonomus2425 extends AbstractAutonomous{
     private double liftTolerance;
     private double liftMin;
     private double armInBasketPos;
-    @Override
 
+    @Override
     public ArrayList<Action2425> makeToDoList() {
         moveTolerance = 2;
         oritentationTolerance = Math.PI/24;
@@ -37,20 +40,18 @@ public class DropOneInAutonomus2425 extends AbstractAutonomous{
         toDoList.add(new GoToPosition2425(driveTrain,new Position(-3*24+9, 2*24+2, Math.PI/2), moveTolerance, positionFinder));
         toDoList.add(new SetLift2425(liftKit, liftMax, liftTolerance));
         toDoList.add(new SetArm2425(clawArm,armLowest,armTolerance)); // Lowers the claw arm
-        toDoList.add(new SetClaw2425(claw, true));
+        toDoList.add(new SetSpinClaw2425(spinClaw, false)); // Pushes the sample out
         //reset for teleop
-        toDoList.add(new SetArm2425(clawArm, raisedArmPos,armTolerance)); // Lowers the claw arm
+        toDoList.add(new SetArm2425(clawArm, raisedArmPos,armTolerance)); // Raises the claw arm
         toDoList.add(new SetLift2425(liftKit, liftMin, liftTolerance));
         toDoList.add(new SetArm2425(clawArm, 0,armTolerance)); // Lowers the claw arm
         //move to 3point area
         toDoList.add(new GoToPosition2425(driveTrain, new Position(24, 0, 0), 3, positionFinder));
-
         return toDoList;
     }
 
     @Override
     public Position getStartPosition() {
-
         return new Position(-3*24+9, 2*24-10, Math.PI/2);
     }
     @Override
