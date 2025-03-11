4e9435d908eb8b5dfa3ce5febc6a7808965b2800
Emmett Alyea <ealyea2028@spyponders.com>
2025-03-11 15:03:39 -0400

    added folder to make gradle tests in. Ignore the ClawArmTest2425.java inside of the directory, it is what I am currently working on.

---
+++ src/test/java/ClawArmTest2425.java
@@ -0,0 +1,12 @@
+/*
+package org.firstinspires.ftc.teamcode.Automotons2425.src.test.java;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class ClawArmTest2425 {
+}
+*/
